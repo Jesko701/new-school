9780bea6228f274376bdd424102c85c89095641a
Jesko701 <jovialpattiasina@gmail.com>
2025-07-01 21:03:26 +0700

    add section at page 'About' with Targets and EndSection components

---
+++ src/app/about/components/Targets.js
@@ -0,0 +1,34 @@
+export function Targets() {
+  return (
+    <div class="container mx-auto">
+      <section className="px-4 py-8 border-t border-gray-200 mt-12">
+        <div class="flex flex-col md:flex-row md:items-center">
+          <div class="md:w-1/4 lg:w-1/5 font-bold text-xl text-gray-800 pr-5 mb-4 md:mb-0">
+            Mission Statement
+          </div>
+          <div class="md:w-3/4 lg:w-4/5 text-lg leading-relaxed text-gray-700">
+            To become an educational institution that is partnered with parents
+            and grows strong islamic generations who own global awareness and
+            determine to build the society
+          </div>
+        </div>
+      </section>
+      <section className="px-4 py-8 border-t border-b border-gray-200 mb-12">
+        <div class="flex flex-col md:flex-row md:items-center">
+          <div class="md:w-1/4 lg:w-1/5 font-bold text-xl text-gray-800 pr-5 mb-4 md:mb-0">
+            Our Core Value
+          </div>
+          <div class="md:w-3/4 lg:w-4/5 text-lg leading-relaxed text-gray-700">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            aliquam aliquid quod tempora saepe, officia dolores error. Quod quos
+            facere corrupti, accusantium dolorum similique tempore, perspiciatis
+            fuga blanditiis explicabo adipisci animi necessitatibus dignissimos
+            sint. Laborum consequatur quaerat autem vitae ipsum. Quasi
+            recusandae illo sint inventore adipisci repellendus facilis vel
+            nobis.
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,15 +1,19 @@
 import { Description } from "./components/Description";
+import { EndSection } from "./components/EndSection";
 import { QuoteSection } from "./components/QuoteSection";
 import { StatsSection } from "./components/StatsSection";
+import { Targets } from "./components/Targets";
 export default function about() {
   return (
-    <div className="flex flex-col items-center  bg-gray-100">
+    <div className="flex flex-col items-center bg-white">
       <div className="w-full bg-white py-7 flex justify-center">
         <h1 className="text-4xl font-bold mb-8 mt-5 text-black">About Us</h1>
       </div>
       <Description />
       <QuoteSection />
       <StatsSection />
+      <Targets />
+      <EndSection />
     </div>
   );
 }
